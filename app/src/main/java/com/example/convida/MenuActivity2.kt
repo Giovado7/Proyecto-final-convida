2c41c1bfe51381ad40ab10c68ed5df5ad558ead6
Giovannni Santiago <72050548+Giovado7@users.noreply.github.com>
2020-12-14 17:58:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/convida/MenuActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.convida
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu2)
+    }
+}
